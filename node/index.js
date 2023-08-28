c654d2376cd2c9bf7b632933bf483048caedc0c7
wonil <33892754+wonil1998@users.noreply.github.com>
2023-08-28 20:56:30 +0900

    Update index.js

--- node/index.js
+++ node/index.js
@@ -1,8 +1,12 @@
 const express = require("express"); //express를 설치했기 때문에 가져올 수 있다.
 const app = express();
 
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000);
+app.listen(PORT, () => console.log("server running.."));
